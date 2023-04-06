95f715196e419a7c1cdf4cc99fcee928b3a865ae
David Jangdal <djangdal@gmail.com>
2023-04-06 10:11:08 +0200

    Created empty project

---
+++ WalkingPhoto/WalkingPhoto/WalkingPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingPhotoApp.swift
+//  WalkingPhoto
+//
+//  Created by David Jangdal on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct WalkingPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
